cc3da096af8f78cfe7441d32a2d301933adb0c91
Frank McAuley <frank.mcauley@gmail.com>
2011-03-04 06:45:38 -0500

    Added the XPathQuery class to parse xml

---
+++ XPathQuery.h
@@ -0,0 +1,16 @@
+//
+//  XPathQuery.h
+//  TCApp
+//
+//  Created by Frank McAuley on 3/4/11.
+//  Copyright 2011 Frank McAuley LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface XPathQuery : NSObject {
+    
+}
+
+@end
---
+++ XPathQuery.m
@@ -0,0 +1,14 @@
+//
+//  XPathQuery.m
+//  TCApp
+//
+//  Created by Frank McAuley on 3/4/11.
+//  Copyright 2011 Frank McAuley LLC. All rights reserved.
+//
+
+#import "XPathQuery.h"
+
+
+@implementation XPathQuery
+
+@end
