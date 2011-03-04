aec3a40f2afac00629f5b01d46d2e72563268b0c
Frank McAuley <frank.mcauley@gmail.com>
2011-03-04 06:50:44 -0500

    Updated the XPathQuery class

--- XPathQuery.h
+++ XPathQuery.h
@@ -5,6 +5,18 @@
 //  Created by Frank McAuley on 3/4/11.
 //  Copyright 2011 Frank McAuley LLC. All rights reserved.
 //
+//
+//  XPathQuery.m
+//  FuelFinder
+//
+//  Created by Matt Gallagher on 4/08/08.
+//
+//  Permission is given to use this source code file, free of charge, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
 
 #import <Foundation/Foundation.h>
 
@@ -12,5 +24,8 @@
 @interface XPathQuery : NSObject {
     
 }
+
+NSArray *PerformHTMLXPathQuery(NSData *document, NSString *query);
+NSArray *PerformXMLXPathQuery(NSData *document, NSString *query);
 
 @end
