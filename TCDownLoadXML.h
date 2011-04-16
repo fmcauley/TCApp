7e40e65ee94449f7bdd767e4c1d855c943fddf30
Frank McAuley <frank.mcauley@gmail.com>
2011-04-15 22:47:12 -0400

    Added a new class TCDownLoadXML for processing the data pulled from the webserver

---
+++ TCDownLoadXML.h
@@ -0,0 +1,16 @@
+//
+//  TCDownLoadXML.h
+//  TCApp
+//
+//  Created by Frank McAuley on 4/15/11.
+//  Copyright 2011 Frank McAuley LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TCDownLoadXML : NSObject {
+    
+}
+
+@end
---
+++ TCDownLoadXML.m
@@ -0,0 +1,14 @@
+//
+//  TCDownLoadXML.m
+//  TCApp
+//
+//  Created by Frank McAuley on 4/15/11.
+//  Copyright 2011 Frank McAuley LLC. All rights reserved.
+//
+
+#import "TCDownLoadXML.h"
+
+
+@implementation TCDownLoadXML
+
+@end
